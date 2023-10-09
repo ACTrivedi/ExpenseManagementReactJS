f642f8fb3385ee69838925cddd3735ae78c6f834
Aakash Trivedi <aakash.trivedi@tatvasoft.com>
2023-10-09 10:39:33 +0530

    ExpenseMangementCommit2

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,7 @@
-import './Card.css';
+import "./Card.css";
 
-const Card=(props)=>{
-    const classes = `card ${props.className}`;
-    return(
-        <div className={classes}>{props.children}</div>
-    );
-}
+const Card = (props) => {
+  const classes = `card ${props.className}`;
+  return <div className={classes}>{props.children}</div>;
+};
 export default Card;
